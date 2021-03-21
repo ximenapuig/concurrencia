d71e1787f5b5515b1a8b11806227c0e5cf7a7ced
nicolas_cossio <nicolas_cossio@pcNico>
2021-03-21 14:10:03 +0100

    no habia concurrencia, ahora si LOL

--- src/CC_04_MutexSem.java
+++ src/CC_04_MutexSem.java
@@ -2,7 +2,7 @@
 import es.upm.babel.cclib.Semaphore;
 
 class CC_04_MutexSem {
-	
+
 	private static Semaphore s = new Semaphore (1);
 	private static int N_THREADS = 2;
 	private static int N_PASOS = 1000000;
@@ -15,7 +15,7 @@ class CC_04_MutexSem {
 		public void dec () { this.n--; }
 
 	}	
-	
+
 	static class Incrementador extends Thread {
 		private Contador cont;
 		public Incrementador (Contador c) {
@@ -23,29 +23,31 @@ class CC_04_MutexSem {
 		}
 		public void run() {
 			System.out.println("Aquiring permit in Incrementador"); 
-			s.await();
 			for (int i = 0; i < N_PASOS; i++) {
+				s.await();
 				this.cont.inc();
+				System.out.println("inc: " + this.cont.valorContador()); 
+				s.signal();
 			}
 			System.out.println("Releasing permit in Incrementador"); 
-			s.signal();
 		}
 	}
 
 	static class Decrementador extends Thread {
 		private Contador cont;
-		
+
 		public Decrementador (Contador c) {
 			this.cont = c;
 		}
 		public void run() {
 			System.out.println("Aquiring permit in Decrementador"); 
-			s.await();
 			for (int i = 0; i < N_PASOS; i++) {
+				s.await();
 				this.cont.dec();
+				System.out.println("dec: " + this.cont.valorContador()); 
+				s.signal();
 			}
 			System.out.println("Releasing permit in Decrementador"); 
-			s.signal();
 		}
 	}
 
